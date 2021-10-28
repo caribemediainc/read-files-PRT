caedd850de5340ccd988fbaeeddb30e7743fad2e
Nadia Disla <tejadanadiam@gmail.com>
2021-10-28 06:41:49 -0700

    Complete file BC.

--- read-files-PRT/Data/BlancasComercial.cs
+++ read-files-PRT/Data/BlancasComercial.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace read_files_PRT.Data
@@ -12,12 +13,16 @@ namespace read_files_PRT.Data
             ControlGeneral generalControl = new ControlGeneral();
             PueblosRegiones pueblosRegiones = new PueblosRegiones();
             generalControl.ValidateFileExistence(ControlGeneral.rutaArchivoWriteBC, ControlGeneral.rutaArchivoReadBC);
-            
+
+            List<string> codeRepeatCommercial = new List<string>();
+            List<string> commercialWrited = new List<string>();
+            int id = 0;
+
             while (!ControlGeneral.streamReader.EndOfStream)
             {
                 Console.WriteLine("Escribiendo archivo...");
                 string line = ControlGeneral.streamReader.ReadLine(), commercialName = "", undefined = "", undefinedCode = "", undefinedCode2 = "", phoneOriginal = "", phone = "", pueblo = "", originalAddress = "", finalAddress = "", newAddress = "",
-                                    undefined2 = "", undefined3 = "", splitColumnText;
+                                    undefined2 = "", undefined3 = "", splitColumnText= "", lastCodeRepeat = "", beforeCodeRepeat = "", lastCommercial = "", finalAddressWrite = "";
 
                 commercialName = line.Substring(0, 42);
                 undefined = line.Substring(42, 17);
@@ -26,12 +31,18 @@ namespace read_files_PRT.Data
                 undefined2 = line.Substring(69, 1);
                 undefined3 = line.Substring(70, 57).TrimStart();
                 pueblo = undefined3.Substring(38, 3);
-                undefined3 = line.Substring(70, 41).Trim().Substring(2);
+                undefined3 = line.Substring(69,51).TrimStart().Substring(2);
+                undefinedCode = undefined3.Substring(39, 3);
+                undefinedCode2 = undefined3.Substring(42, 3);
+
                 if (line.Length > 129)
                 {
                     finalAddress = line.Substring(129);
                 }
-                else { finalAddress = ""; }
+                else 
+                { 
+                    finalAddress = ""; 
+                }
                 /*if (finalAddress.ToLower().Equals("ext"))
                 {
                     phone = "*" + phoneOriginal.Substring(6);
@@ -99,12 +110,96 @@ namespace read_files_PRT.Data
                         }
                     }
                 }
+                pueblo = pueblo.Replace(';', 'Ñ');
+                pueblo = pueblo.TrimEnd();
+                commercialName = commercialName.Replace(';', 'ñ');
                 pueblosRegiones.PuebloRegion(pueblo);
-                ControlGeneral.streamWriter.Write($"{commercialName}|{undefined}|{phone}|{undefined2}|{pueblo}|{undefinedCode}|{undefinedCode2}|");
-                foreach (string dir in info2)
+                string repeatCommercial = undefined.ToString().Substring(4, 4);
+                codeRepeatCommercial.Add(repeatCommercial);
+                id++;
+                if (commercialWrited.Count > 0) { lastCommercial = commercialWrited.Last(); }
+                if (codeRepeatCommercial.Count > 1)
                 {
-                    ControlGeneral.streamWriter.Write($"{dir}");
+                    lastCodeRepeat = codeRepeatCommercial.Last();
+                    beforeCodeRepeat = codeRepeatCommercial.ElementAt(codeRepeatCommercial.Count - 2);
+
+                    if (lastCodeRepeat == beforeCodeRepeat)
+                    {
+                        ControlGeneral.streamWriter.Write($"{id}|{lastCommercial.Replace(';', 'ñ')}{phone}|{PueblosRegiones.Pueblo}|{PueblosRegiones.Region}|");
+                    }
+                    else
+                    {
+                        ControlGeneral.streamWriter.Write($"{id}|{info2[2].Replace(';', 'ñ')}{phone}|{PueblosRegiones.Pueblo}|{PueblosRegiones.Region}|");
+                        commercialWrited.Add(info2[2]);
+                    }
                 }
+                else
+                {
+                    ControlGeneral.streamWriter.Write($"{id}|{info2[2].Replace(';', 'ñ')}{phone}|{PueblosRegiones.Pueblo}|{PueblosRegiones.Region}|");
+                    commercialWrited.Add(info2[2]);
+                }
+
+                if (undefinedCode2 == "BIO")
+                {
+                    info2[10] = info2[10].Insert(0, "Bo ");
+                }
+                if (undefinedCode == "AVE" || undefinedCode == "CAL" || undefinedCode == "BIO" || undefinedCode == "CAR" || info2.Count >= 12)
+                {
+                    info2[8] = info2[8].TrimEnd('|');
+                    if (undefinedCode == "   " && info2[9] == "|")
+                    {
+                        ControlGeneral.streamWriter.Write(info2[2].Replace(';', 'ñ'));
+                    }
+                    else
+                    {
+                        switch (undefinedCode)
+                        {
+                            case "AVE":
+                                info2[8] = $"{info2[8]} Ave {info2[9]}";
+                                ControlGeneral.streamWriter.Write(info2[8].Replace(';', 'ñ'));
+                                break;
+                            case "CAL":
+                                info2[8] = $"{info2[8].TrimEnd()} Calle {info2[9]}";
+                                ControlGeneral.streamWriter.Write(info2[8].Replace(';', 'ñ'));
+                                break;
+                            case "BIO":
+                                info2[10] = info2[10].Insert(0, "Bo ");
+                                break;
+                            case "CAR":
+                                if (undefinedCode == "CAR" && undefinedCode2 == "PAR")
+                                {
+                                    info2[9] = $"Carr {info2[9].TrimEnd('|')} {info2[10]}";
+                                    info2[10] = "|";
+                                    finalAddressWrite = $"{info2[9].TrimEnd('|')} {info2[10]}";
+                                    ControlGeneral.streamWriter.Write(finalAddressWrite.Replace(';', 'ñ'));
+                                }
+                                else
+                                {
+                                    info2[9] = $"Carr {info2[9]}";
+                                    finalAddressWrite = $"{info2[9].TrimEnd('|')} {info2[10]}";
+                                    ControlGeneral.streamWriter.Write(finalAddressWrite.Replace(';', 'ñ'));
+                                }
+                                break;
+                            case "   ":
+                                if (undefinedCode == "   " && undefinedCode2 == "   ")
+                                {
+                                    info2[8] = $"{info2[8].TrimEnd('|')}{info2[9]}";
+                                    ControlGeneral.streamWriter.Write(info2[8].Replace(';', 'ñ'));
+                                }
+                                else
+                                {
+                                    info2[8] = $"{info2[8].TrimEnd('|')}{info2[9]}";
+                                }
+                                break;
+                        }
+                    }
+                    if (undefinedCode2 == "URB")
+                    {
+                        info2[8] = $"{info2[8].TrimEnd('|')} Urb {info2[10]}";
+                        ControlGeneral.streamWriter.Write(info2[8].Replace(';', 'ñ'));
+                    }
+                }
+
                 ControlGeneral.streamWriter.WriteLine();
             }
             ControlGeneral.streamWriter.Close();
